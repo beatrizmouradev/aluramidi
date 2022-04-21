afe64dbdd8df15a7d5fed5b20f67ef8e7572123d
beatrizmouradev <beatrizmouradev@gmail.com>
2022-04-21 13:39:04 -0300

    lógica aluramid finalizada

---
+++ main.js
@@ -0,0 +1,46 @@
+function tocaSom (idElementoAudio) {
+    
+    const elemento = document.querySelector(idElementoAudio).play()
+    
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play()
+    } else {
+        alert("elemento não encontrado ou seletor inválido")
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+
+    const idAudio = `#som_${instrumento}`
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space'|| evento.code === 'Enter') {
+        tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+
+    }
+}
+
+
+/*Função individual (é inviável criar pra cada um)
+
+function tocaSomPom() {
+
+    document.querySelector("#som_tecla_pom").play()
+}
+
+document.querySelector(".tecla_pom").onclick = tocaSomPom
+*/
